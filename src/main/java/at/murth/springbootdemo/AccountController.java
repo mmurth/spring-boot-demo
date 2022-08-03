2577d3814cc8e3af6bdca728e51e1ab503ae0d05
martinmurth <martin.murth@gmail.com>
2022-08-03 10:56:31 +0200

    Added Cors-Support.

--- src/main/java/at/murth/springbootdemo/AccountController.java
+++ src/main/java/at/murth/springbootdemo/AccountController.java
@@ -1,12 +1,14 @@
 package at.murth.springbootdemo;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
 
+    @CrossOrigin(origins="http://localhost:8080", maxAge = 3600L)
     @GetMapping("/account")
     public String getBallance() {
         return "Your are poor.";
